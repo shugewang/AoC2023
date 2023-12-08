53e4c325b2364ab73e151bacde2a26d68e7b917e
Shu Wang <shu.wang@autotrader.co.uk>
2023-12-08 12:12:42 +0000

    Add suspiciously simple day 6 part 2 solution

--- src/main/kotlin/BoatGame.kt
+++ src/main/kotlin/BoatGame.kt
@@ -1,31 +1,31 @@
 // Day 6
 class BoatGame(val input: MutableList<String>) {
-    private val timeAndDistanceRecords: MutableMap<Int, Int> = getGameRecords()
+    private val timeAndDistanceRecords: MutableMap<Long, Long> = getGameRecords()
 
     private fun parse(index: Int): List<String> {
-        return input[index].split(":")[1].trim().split(" ").filterNot { it == "" }
+        return input[index].split(":")[1].replace(" ", "").split(" ").filterNot { it == "" }
     }
 
-    private fun getGameRecords(): MutableMap<Int, Int> {
-        val timeAndDistanceRecords = mutableMapOf<Int, Int>()
+    private fun getGameRecords(): MutableMap<Long, Long> {
+        val timeAndDistanceRecords = mutableMapOf<Long, Long>()
         val times = parse(0)
         val distances = parse(1)
         for (i in times.indices) {
-            timeAndDistanceRecords[times[i].toInt()] = distances[i].toInt()
+            timeAndDistanceRecords[times[i].toLong()] = distances[i].toLong()
         }
         return timeAndDistanceRecords
     }
 
-    fun multiplyNumberOfWaysToWin(): Int {
+    fun multiplyNumberOfWaysToWin(): Long {
         var product = 1
         timeAndDistanceRecords.forEach() {
             product *= findNumberOfWaysToWin(it).size
         }
-        return product
+        return product.toLong()
     }
 
-    private fun findNumberOfWaysToWin(game: Map.Entry<Int, Int>): MutableList<Int> {
-        var holdToWin = mutableListOf<Int>()
+    private fun findNumberOfWaysToWin(game: Map.Entry<Long, Long>): MutableList<Long> {
+        var holdToWin = mutableListOf<Long>()
         for (second in 1..game.key) {
             if (calculateDistanceTravelled(game.key, second) > game.value) {
                 holdToWin.add(second)
@@ -34,7 +34,7 @@ class BoatGame(val input: MutableList<String>) {
         return holdToWin
     }
 
-    private fun calculateDistanceTravelled(time:Int, holdFor: Int): Int {
+    private fun calculateDistanceTravelled(time:Long, holdFor: Long): Long {
         return (time - holdFor) * holdFor
     }
 }
